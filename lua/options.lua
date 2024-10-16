93a5b9544ce1be2d4920a18f405f080ddf857318
Rococcoon <spacedangles@gmail.com>
2024-10-15 19:14:28 -0700

    add custom opts

--- lua/options.lua
+++ lua/options.lua
@@ -4,3 +4,15 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+--
+vim.opt.relativenumber = true
+
+-- Add transparency where desired
+vim.cmd([[
+	hi Normal guibg=NONE ctermbg=NONE
+	hi NormalNC guibg=NONE ctermbg=NONE
+	hi NormalSB guibg=NONE ctermbg=NONE
+	hi NormalFloat guibg=NONE ctermbg=NONE
+	hi FloatBorder guibg=NONE ctermbg=NONE
+	hi SignColumn guibg=NONE ctermbg=NONE
+]])
